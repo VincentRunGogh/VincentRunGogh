1d51345c071c958446a57cb8d1c8fd04c94c3b2e
ayoung-kim <ayoung7777@gmail.com>
2024-09-12 11:16:30 +0900

    [FEAT][BE][아영] board entity 생성

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/board/entity/Board.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/board/entity/Board.java
@@ -1,4 +1,50 @@
 package com.vincentrungogh.domain.board.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "boards")
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "is_delete")
+    private boolean isDelete;
+
+    @Column(name = "like_count")
+    private int likeCount;
+
+    @Column(name = "created")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime created;
+
+    @Builder
+    private Board(LocalDateTime created, String comment, boolean isDelete, int likeCount) {
+        this.comment = comment;
+        this.isDelete = isDelete;
+        this.likeCount = likeCount;
+        this.created = created;
+    }
+
+    public static Board createBoard(String comment, boolean isDelete, int likeCount) {
+        return Board.builder()
+                .comment(comment)
+                .isDelete(isDelete)
+                .likeCount(likeCount)
+                .created(LocalDateTime.now())
+                .build();
+    }
 }
