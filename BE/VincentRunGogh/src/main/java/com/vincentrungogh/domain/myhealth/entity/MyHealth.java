3488185e220ce018eeca5fa5a4448b97db9590af
ayoung-kim <ayoung7777@gmail.com>
2024-09-12 14:02:53 +0900

    [FIX][BE][아영] myhealth entity create 시 수정

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/myhealth/entity/MyHealth.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/myhealth/entity/MyHealth.java
@@ -18,15 +18,19 @@ public class MyHealth {
     @Column(name = "id")
     private int id;
 
+    // 총 운동 시간
     @Column(name = "total_time")
     private int totalTime;
 
+    // 총 운동 거리
     @Column(name = "total_distance")
     private int totalDistance;
 
+    // 평균 속력
     @Column(name = "average_speed")
     private double averageSpeed;
 
+    // 총 걸음 수
     @Column(name = "total_step")
     private int totalStep;
 
@@ -43,16 +47,17 @@ public class MyHealth {
         this.user = user;
     }
 
-    public static MyHealth createMyHealth(int totalTime, int totalDistance, double averageSpeed, int totalStep, User user) {
+    public static MyHealth createMyHealth(User user) {
         return MyHealth.builder()
-                .totalTime(totalTime)
-                .totalDistance(totalDistance)
-                .averageSpeed(averageSpeed)
-                .totalStep(totalStep)
+                .totalTime(0)
+                .totalDistance(0)
+                .averageSpeed(0)
+                .totalStep(0)
                 .user(user)
                 .build();
     }
 
+    // 총 운동 시간/거리, 평균 속력, 총 걸을 수 업데이트
     public void updateMyHealth(int totalTime, int totalDistance, double averageSpeed, int totalStep){
         this.totalTime = totalTime;
         this.totalDistance = totalDistance;
