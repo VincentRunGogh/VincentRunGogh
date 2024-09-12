2eaa4fb69f1f927feca621a68e91bbbaa16eff0f
이석범 <dltjrqja01@naver.com>
2024-09-12 17:07:52 +0900

    [FEAT][BE][석범] 임시로 전체 접근 허용

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/config/SecurityConfig.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> authorize
                         //모두 접근 허용
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
+                        .requestMatchers("/**","/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
                         //유저만 접근 허용
                         .anyRequest().authenticated())
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -43,6 +43,6 @@ public class SecurityConfig {
         // 아래 url은 filter 에서 제외
         return web ->
                 web.ignoring()
-                        .requestMatchers("/login/**", "/token/refresh", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**");
+                        .requestMatchers("/**", "/login/**", "/token/refresh", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**");
     }
 }
