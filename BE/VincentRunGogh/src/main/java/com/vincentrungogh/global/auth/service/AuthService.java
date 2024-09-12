92052f64eb500357bae1e15b55fd4f303409e90e
ayoung-kim <ayoung7777@gmail.com>
2024-09-12 16:44:52 +0900

    [FEAT][BE][아영] AuthService 생성

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/auth/service/AuthService.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/auth/service/AuthService.java
@@ -1,4 +1,16 @@
 package com.vincentrungogh.global.auth.service;
 
+import com.vincentrungogh.global.auth.service.dto.request.LoginRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class AuthService {
+
+    public void login(LoginRequest loginRequest) {
+    }
 }
