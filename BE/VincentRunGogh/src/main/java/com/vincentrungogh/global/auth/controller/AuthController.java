9750bafb1b73cc6ff27fd1923ef13332335386f1
ayoung-kim <ayoung7777@gmail.com>
2024-09-15 22:48:39 +0900

    [FEAT][BE][아영] 로그인 API 구현

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/auth/controller/AuthController.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/auth/controller/AuthController.java
@@ -3,13 +3,16 @@ package com.vincentrungogh.global.auth.controller;
 
 import com.vincentrungogh.global.auth.service.AuthService;
 import com.vincentrungogh.global.auth.service.dto.request.LoginRequest;
+import com.vincentrungogh.global.auth.service.dto.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -19,6 +22,9 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
-        authService.
+
+        log.info("AuthController : 로그인 시작");
+        LoginResponse response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
     }
 }
--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/auth/service/AuthService.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/auth/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.vincentrungogh.global.auth.service;
 
 import com.vincentrungogh.global.auth.service.dto.request.LoginRequest;
+import com.vincentrungogh.global.auth.service.dto.response.LoginResponse;
+import com.vincentrungogh.global.auth.service.dto.response.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +15,25 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
 
-    public void login(LoginRequest loginRequest) {
+    private final AuthenticationManager authorizationManager;
+    private final JwtService jwtService;
+
+    public LoginResponse login(LoginRequest loginRequest) {
+        log.info("AuthService : 로그인 시작");
+        Authentication authentication = authorizationManager.authenticate(
+                // 아이디, 패스워드 입력
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        String accessToken = jwtService.buildAccessToken(userPrincipal.getId());
+        String refreshToken = jwtService.buildRefreshToken(userPrincipal.getId());
+
+        return LoginResponse.createLoginResponse(
+                accessToken,refreshToken
+        );
     }
 }
