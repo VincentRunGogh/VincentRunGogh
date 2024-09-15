76e4be3b34a7d9b857df98480db677625b9bb86a
ayoung-kim <ayoung7777@gmail.com>
2024-09-15 22:49:34 +0900

    [FEAT][BE][아영] user id 및 email 기준 조회 구현

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/user/service/UserService.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/user/service/UserService.java
@@ -1,4 +1,38 @@
 package com.vincentrungogh.domain.user.service;
 
-public class UserService {
+import com.vincentrungogh.domain.user.entity.User;
+import com.vincentrungogh.domain.user.repository.UserRepository;
+import com.vincentrungogh.global.auth.service.dto.response.UserPrincipal;
+import com.vincentrungogh.global.exception.CustomException;
+import com.vincentrungogh.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public User getUserById(int userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        try{
+            // 1. 사용자 확인
+            User user = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+            return UserPrincipal.createUserPrincipal(user.getId(), user.getEmail(), user.getPassword());
+        } catch (Exception e){
+            throw new CustomException(ErrorCode.JSON_PROCESSING_ERROR);
+        }
+    }
 }
