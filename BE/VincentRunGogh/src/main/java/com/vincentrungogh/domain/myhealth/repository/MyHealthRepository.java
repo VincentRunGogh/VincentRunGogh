4ca94ae5faedefd95daf8d9c98caf30b74cd83d4
이석범 <dltjrqja01@naver.com>
2024-09-17 00:30:32 +0900

    [FEAT][BE][석범] 유저로 myHealth찾기 추가

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/myhealth/repository/MyHealthRepository.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/myhealth/repository/MyHealthRepository.java
@@ -1,4 +1,13 @@
 package com.vincentrungogh.domain.myhealth.repository;
 
-public interface MyHealthRepository {
+import com.vincentrungogh.domain.myhealth.entity.MyHealth;
+import com.vincentrungogh.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MyHealthRepository extends JpaRepository<MyHealth, Integer> {
+    Optional<MyHealth> findByUser(User user);
 }
