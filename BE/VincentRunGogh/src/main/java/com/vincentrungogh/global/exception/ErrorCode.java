2416e9a8fad9f7be3708ed3dd53081fcd7b553b8
이석범 <dltjrqja01@naver.com>
2024-09-17 00:28:18 +0900

    [FEAT][BE][석범] 에러코드 추가

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/exception/ErrorCode.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/exception/ErrorCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 //에러 코드 모음집
 //사용법 에러명("message", 실제 에러상태)
 public enum ErrorCode {
-    UNAUTHORIZED("유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED)
+    UNAUTHORIZED("유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED),
+    FAILED_CHANGE_ROOTING("루트화 하는데 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    FAILED_SAVE_ROOTING("루트르 저장하는데 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR)
     ;
 
     private final String message;
