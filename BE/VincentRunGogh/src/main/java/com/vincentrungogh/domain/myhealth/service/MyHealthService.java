31ffa63504abcd3b5580a54646964a784dbb54ff
이석범 <dltjrqja01@naver.com>
2024-09-17 10:56:28 +0900

    [FEAT][BE][석범] 사용자의 헬스 정보 찾기

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/myhealth/service/MyHealthService.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/myhealth/service/MyHealthService.java
@@ -1,4 +1,21 @@
 package com.vincentrungogh.domain.myhealth.service;
 
+import com.vincentrungogh.domain.myhealth.entity.MyHealth;
+import com.vincentrungogh.domain.myhealth.repository.MyHealthRepository;
+import com.vincentrungogh.domain.user.entity.User;
+import com.vincentrungogh.global.exception.CustomException;
+import com.vincentrungogh.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MyHealthService {
+    private final MyHealthRepository myHealthRepository;
+
+    public MyHealth getMyHealth(User user) {
+        return myHealthRepository.findByUser(user).orElseThrow(
+                () -> new CustomException(ErrorCode.USER_NOT_FOUND)
+        );
+    }
 }
