79699500a0a900a2314a1b1901a85633cca77533
이석범 <dltjrqja01@naver.com>
2024-09-19 16:58:08 +0900

    [DEL][DATA][석범] mongoDB 연결

--- DATA/VincentRunGogh/main.py
+++ DATA/VincentRunGogh/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
 from api.RouteAPI import api_router  # routeAPI 파일에서 라우터를 가져옵니다.
+from db import mongodb
+
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_app_start():
+	mongodb.connect()
+
+@app.on_event("shutdown")
+async def on_app_shutdown():
+	mongodb.close()
 
 # "/api/v1"를 기본 경로로 설정하여 라우터 등록
 app.include_router(api_router, prefix="/api/v1")
