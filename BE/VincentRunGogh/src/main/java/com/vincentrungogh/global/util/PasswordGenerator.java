ca1a0576381d1b6b73f133a0ab610ff4e3c4570c
ayoung-kim <ayoung7777@gmail.com>
2024-09-20 16:21:19 +0900

    [FEAT][BE][아영] 패스워드 생성 및 dto 구현

---
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/util/PasswordGenerator.java
@@ -0,0 +1,20 @@
+package com.vincentrungogh.global.util;
+
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    private static final int PASSWORD_LENGTH = 12;
+
+    public static String generateRandomPassword() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            password.append(CHARACTERS.charAt(randomIndex));
+        }
+
+        return password.toString();
+    }
+}
