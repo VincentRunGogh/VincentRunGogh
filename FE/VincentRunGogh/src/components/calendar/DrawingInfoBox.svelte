35767707672eaf95308a8b48b650317cd33f2f3b
Jeon-YJ1004 <dudwn5374@naver.com>
2024-09-20 17:43:39 +0900

    [FIX][FE][영주] 캘린더 커밋 revert, 캘린더 상세 완

--- FE/VincentRunGogh/src/components/calendar/DrawingInfoBox.svelte
+++ FE/VincentRunGogh/src/components/calendar/DrawingInfoBox.svelte
@@ -11,17 +11,9 @@
 <div>
   <div class="space-y-4">
     <Card img={imgSrc} horizontal size="md">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Noteworthy technology acquisitions 2021
-      </h5>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-        chronological order.
-      </p>
+      <DrawingSummaryInfo {time} {averagePace} {distance} />
     </Card>
   </div>
-  <img src={imgSrc} alt="드로잉 캡쳐 이미지" />
-  <DrawingSummaryInfo {time} {averagePace} {distance} />
 </div>
 
 <style>
