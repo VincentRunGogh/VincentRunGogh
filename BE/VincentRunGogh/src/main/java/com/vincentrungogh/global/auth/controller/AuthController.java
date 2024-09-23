d0c6fed3bacc7b2a8bc2ef48b4feed9855b9f4bf
ayoung-kim <ayoung7777@gmail.com>
2024-09-23 12:37:16 +0900

    [FEAT][BE][아영] cross origin 설정

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/auth/controller/AuthController.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/auth/controller/AuthController.java
@@ -23,6 +23,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@CrossOrigin()
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
