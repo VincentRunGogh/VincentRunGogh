0fe514ecb65d43d4256675b53685baf4c4a4a575
ayoung-kim <ayoung7777@gmail.com>
2024-09-23 12:37:05 +0900

    [FEAT][BE][아영] 프로필 조회  API 구현

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/user/service/UserService.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.vincentrungogh.domain.user.service;
 
 import com.vincentrungogh.domain.user.entity.User;
 import com.vincentrungogh.domain.user.repository.UserRepository;
+import com.vincentrungogh.domain.user.service.dto.request.UpdateUserProfileRequest;
 import com.vincentrungogh.domain.user.service.dto.response.UserProfileResponse;
 import com.vincentrungogh.global.auth.service.dto.response.UserPrincipal;
 import com.vincentrungogh.global.exception.CustomException;
@@ -13,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -23,12 +26,32 @@ public class UserService implements UserDetailsService {
     public UserProfileResponse getUserProfile(int userId){
 
         // 1. 유저 찾기
-        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        User user = getUserById(userId);
 
         // 2. 반환
         return UserProfileResponse.createUserProfileResponse(user.getNickname(), user.getGender(),
                 String.valueOf(user.getBirth()), user.getProfile(), user.getHeight(), user.getWeight());
 
+    }
+
+    public void updateUserProfile(int userId, UpdateUserProfileRequest request){
+        // 1. 키 몸무게 0인지 확인
+        if(request.getHeight() * request.getWeight() == 0){
+            throw new CustomException(ErrorCode.FAILED_UPDATE_PROFILE);
+        }
+
+        // 2. 닉네임 중복 확인
+        Optional<User> optionalUser = userRepository.findByNickname(request.getNickname());
+        if(optionalUser.isPresent()){
+            throw new CustomException(ErrorCode.DUPLICATED_NICKNAME);
+        }
+
+        // 3. 유저 찾기
+        User user = getUserById(userId);
+
+        // 4. 저장
+        user.updateProfile(request.getNickname(), request.getWeight(), request.getHeight());
+        userRepository.save(user);
     }
 
 
