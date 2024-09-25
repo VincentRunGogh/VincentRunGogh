90d0f9a050fa6b1802a5a5b9b3afdea9e370220e
ayoung-kim <ayoung7777@gmail.com>
2024-09-25 11:18:34 +0900

    [FEAT][BE][아영] Socket congfig 구현

---
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/drawing/handler/StompHandler.java
@@ -0,0 +1,48 @@
+package com.vincentrungogh.domain.drawing.handler;
+
+import com.vincentrungogh.global.auth.service.JwtService;
+import com.vincentrungogh.global.exception.CustomException;
+import com.vincentrungogh.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+
+@RequiredArgsConstructor
+@Component
+public class StompHandler implements ChannelInterceptor {
+
+    private final JwtService jwtService;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        // 1. 토큰 존재 여부 확인
+        if(accessor.getCommand() == StompCommand.CONNECT){
+            if(jwtService.validateToken(accessor.getFirstNativeHeader("Authorization"))){
+                throw new CustomException(ErrorCode.USER_NOT_FOUND);
+            }
+        }
+
+        return message;
+    }
+
+    @EventListener(SessionConnectedEvent.class)
+    public void onApplicationEvent(SessionConnectedEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String accessToken = accessor.getFirstNativeHeader("Authorization");
+
+        if(!jwtService.validateToken(accessToken)) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        accessor.getSessionAttributes().put("id", jwtService.getUserId(accessToken));
+    }
+
+}
---
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/global/config/WebSocketConfig.java
@@ -0,0 +1,37 @@
+package com.vincentrungogh.global.config;
+
+import com.vincentrungogh.domain.drawing.handler.StompHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();;
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 구독 요청
+        registry.enableSimpleBroker("/sub");
+        // 발행 요청
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
+}
