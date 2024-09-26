2fd1c2b66600a9d42e5748a2aa57d365c67a9dae
ayoung-kim <ayoung7777@gmail.com>
2024-09-26 17:12:16 +0900

    [FEAT][BE][아영] 드로잉 엔티티 수정

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/drawing/service/DrawingService.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/drawing/service/DrawingService.java
@@ -1,8 +1,5 @@
 package com.vincentrungogh.domain.drawing.service;
 
-import com.vincentrungogh.domain.drawing.repository.DrawingRepository;
-import com.vincentrungogh.domain.drawing.service.dto.request.DrawingRealTimeRequest;
-import com.vincentrungogh.global.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,10 +9,4 @@ import org.springframework.stereotype.Service;
 @Service
 public class DrawingService {
 
-    private final RedisService redisService;
-    private final DrawingRepository drawingRepository;
-
-    public void saveDrawingRealTime(int userId, DrawingRealTimeRequest request){
-        redisService.saveDrawingRealTime(userId, request);
-    }
 }
