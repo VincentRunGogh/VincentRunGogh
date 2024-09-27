0419a143c0d6f0d746d0f6946bcde5ab6604fe71
ayoung-kim <ayoung7777@gmail.com>
2024-09-27 16:26:00 +0900

    [FEAT][BE][아영] 드로잉 저장 API 구현

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/drawing/entity/DrawingDetail.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/drawing/entity/DrawingDetail.java
@@ -1,5 +1,7 @@
 package com.vincentrungogh.domain.drawing.entity;
 
+import com.vincentrungogh.domain.drawing.service.dto.request.SaveDrawingRequest;
+import com.vincentrungogh.domain.drawing.service.dto.response.DataSaveDrawingDetailResponse;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -51,7 +53,8 @@ public class DrawingDetail {
     private Drawing drawing;
 
     @Builder
-    private DrawingDetail(Boolean isCompleted, int time, int distance, double speed, int step, String currentDrawingImage, LocalDateTime created, Drawing drawing) {
+    private DrawingDetail(String id, Boolean isCompleted, int time, int distance, double speed, int step, String currentDrawingImage, LocalDateTime created, Drawing drawing) {
+        this.id = id;
         this.isCompleted = isCompleted;
         this.time = time;
         this.distance = distance;
@@ -59,37 +62,38 @@ public class DrawingDetail {
         this.step = step;
         this.currentDrawingImage = currentDrawingImage;
         this.created = created;
+        this.drawing = drawing;
     }
 
-    public static DrawingDetail createDrawingDetail(Drawing drawing){
+    public static DrawingDetail createDrawingDetail(DataSaveDrawingDetailResponse data,
+                                                    String image, Drawing drawing){
         return DrawingDetail.builder()
+                .id(data.getDrawingDetailId())
                 .isCompleted(false)
-                .time(0)
-                .distance(0)
-                .speed(0)
-                .step(0)
-                .currentDrawingImage(null)
+                .time(data.getTime())
+                .distance(data.getDistance())
+                .speed(data.getSpeed())
+                .step(data.getDistance())
+                .currentDrawingImage(image)
                 .created(LocalDateTime.now())
                 .drawing(drawing)
                 .build();
+
     }
 
-    // 드로잉이 일시 정지 되었을 때
-    public void stopDrawing(int time, int distance, int speed, int step, String currentDrawingImage){
-        this.time = time;
-        this.distance = distance;
-        this.speed = speed;
-        this.step = step;
-        this.currentDrawingImage = currentDrawingImage;
-    }
+    public static DrawingDetail completeDrawingDetail(DataSaveDrawingDetailResponse data,
+                                                    String image, Drawing drawing){
+        return DrawingDetail.builder()
+                .id(data.getDrawingDetailId())
+                .isCompleted(true)
+                .time(data.getTime())
+                .distance(data.getDistance())
+                .speed(data.getSpeed())
+                .step(data.getDistance())
+                .currentDrawingImage(image)
+                .created(LocalDateTime.now())
+                .drawing(drawing)
+                .build();
 
-    // 드로잉이 완료되었을 때
-    public void completeDrawing(int time, int distance, int speed, int step, String currentDrawingImage){
-        this.time = time;
-        this.distance = distance;
-        this.speed = speed;
-        this.step = step;
-        this.currentDrawingImage = currentDrawingImage;
-        this.isCompleted = true;
     }
 }
