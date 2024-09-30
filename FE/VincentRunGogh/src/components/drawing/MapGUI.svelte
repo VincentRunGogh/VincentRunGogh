f714da90078fb9987bb8829737a120302e2a1dcf
Jeon-YJ1004 <dudwn5374@naver.com>
2024-09-30 17:58:07 +0900

    [STYLE][FE] 드로잉  페이지 gui

--- FE/VincentRunGogh/src/components/drawing/MapGUI.svelte
+++ FE/VincentRunGogh/src/components/drawing/MapGUI.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { createEventDispatcher } from 'svelte';
-
   import {
     isLockScreen,
     isPause,
@@ -22,96 +21,85 @@
 
   let isMute = false;
   let hasRoute = false;
-  //러닝 정보(시간, 총 길이, 평균 속도, 남은 거리)
+
   function clickPause() {
     dispatch('click-pause', $isPause);
   }
+
   function clickMute() {
     isMute = !isMute;
     dispatch('click-mute', isMute);
   }
+
   function clickLockScreen() {
     dispatch('click-lockScreen', $isLockScreen);
   }
 </script>
 
-<div class="leaflet-bottom-center">
-  <div>
-    <!-- 화면 잠금 -->
-    {#if $isLockScreen}
-      <button type="button" on:click={clickLockScreen} class="lock-btn">
+<div class="flex items-center justify-center">
+  <div class="relative bg-white shadow-lg rounded-lg p-4">
+    <button
+      class="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-yellow-500 p-2 rounded-full focus:outline-none"
+      on:click={clickPause}
+      aria-label="Pause Button"
+    >
+      <!-- Pause icon -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6" />
+      </svg>
+    </button>
+
+    <div class="flex items-center justify-between">
+      <button
+        class="text-green-500 focus:outline-none"
+        on:click={clickMute}
+        aria-label="Mute Button"
+      >
+        <!-- Mute icon -->
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="2.2em"
-          height="2.2em"
+          class="h-8 w-8"
+          fill="none"
           viewBox="0 0 24 24"
-          {...$$props}
+          stroke="currentColor"
         >
-          <path
-            fill="black"
-            d="M9.85 16q-.35 0-.6-.25t-.25-.6v-3.3q0-.35.25-.6t.6-.25H10v-1q0-.825.588-1.412T12 8t1.413.588T14 10v1h.15q.35 0 .6.25t.25.6v3.3q0 .35-.25.6t-.6.25zM11 11h2v-1q0-.425-.287-.712T12 9t-.712.288T11 10zM7 23q-.825 0-1.412-.587T5 21V3q0-.825.588-1.412T7 1h10q.825 0 1.413.588T19 3v18q0 .825-.587 1.413T17 23zm0-5h10V6H7z"
-          />
-        </svg>
-      </button>
-    {:else}
-      <!-- 네비 소리 버튼 -->
-      <button type="button" on:click={clickMute}>
-        {#if isMute}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="2em"
-            height="2em"
-            viewBox="0 0 24 24"
-            {...$$props}
-          >
+          {#if isMute}
             <path
               fill="#FF4040"
               fill-rule="evenodd"
               d="M17.47 9.47a.75.75 0 0 1 1.06 0L20 10.94l1.47-1.47a.75.75 0 0 1 1.06 1.06L21.061 12l1.47 1.47a.75.75 0 0 1-1.061 1.06L20 13.06l-1.47 1.47a.75.75 0 0 1-1.06-1.06L18.94 12l-1.47-1.47a.75.75 0 0 1 0-1.06m-4.433-6.074c1.163-.767 2.713.068 2.713 1.461v14.286c0 1.394-1.55 2.228-2.713 1.461l-6-3.955a.25.25 0 0 0-.137-.042H4a2.75 2.75 0 0 1-2.75-2.75v-3.714A2.75 2.75 0 0 1 4 7.393h2.9a.25.25 0 0 0 .138-.041z"
               clip-rule="evenodd"
             />
-          </svg>
-        {:else}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="2em"
-            height="2em"
-            viewBox="0 0 24 24"
-            {...$$props}
-          >
+          {:else}
             <path
               fill="currentColor"
               fill-rule="evenodd"
-              d="M18.97 6.97a.75.75 0 0 1 1.06 0l-.53.53l.53-.53h.001l.001.002l.003.002l.007.007l.02.02l.062.069c.05.057.12.138.201.241A6.87 6.87 0 0 1 21.75 11.5a6.87 6.87 0 0 1-1.425 4.189a5 5 0 0 1-.264.31l-.02.02l-.006.007l-.003.002v.001h-.001l-.51-.508l.51.51a.75.75 0 1 1-1.061-1.061l.53.53l-.53-.53h-.001v.001l-.002.001l.005-.005l.033-.036q.048-.052.139-.167a5.37 5.37 0 0 0 .448-5.843a5 5 0 0 0-.448-.685a3 3 0 0 0-.172-.203l-.005-.005a.75.75 0 0 1 .003-1.058m-5.933-3.574c1.163-.767 2.713.068 2.713 1.461v14.286c0 1.394-1.55 2.228-2.713 1.461l-6-3.955a.25.25 0 0 0-.137-.042H4a2.75 2.75 0 0 1-2.75-2.75v-3.714A2.75 2.75 0 0 1 4 7.393h2.9a.25.25 0 0 0 .138-.041z"
+              d="M18.97 6.97a.75.75 0 0 1 1.06 0l-.53.53l.53-.53h.001l.001.002l.003.002l.007.007l.02.02l.062.069c.05.057.12.138.201.241A6.87 6.87 0 0 1 21.75 11.5a6.87 6.87 0 0 1-1.425 4.189a5 5 0 0 1-.264.31l-.02.02l-.006.007l-.003.002v.001h-.001l-.51-.508l.51.51 a.75.75 0 1 1-1.061-1.061l.53.53l-.53-.53h-.001v.001l-.002.001l.005-.005l.033-.036q.048-.052.139-.167a5.37 5.37 0 0 0 .448-5.843a5 5 0 0 0-.448-.685a3 3 0 0 0-.172-.203l-.005-.005a.75.75 0 0 1 .003-1.058m-5.933-3.574c1.163-.767 2.713.068 2.713 1.461v14.286c0 1.394-1.55 2.228-2.713 1.461l-6-3.955a.25.25 0 0 0-.137-.042H4a2.75 2.75 0 0 1-2.75-2.75v-3.714A2.75 2.75 0 0 1 4 7.393h2.9a.25.25 0 0 0 .138-.041z"
               clip-rule="evenodd"
             />
-          </svg>
-        {/if}
-      </button>
-      <!-- 일시정지 버튼 -->
-      <button type="button" on:click={clickPause}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="2em"
-          height="2em"
-          viewBox="0 0 24 24"
-          {...$$props}
-        >
-          <path
-            fill="currentColor"
-            fill-rule="evenodd"
-            d="M8 5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2zm7 0a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z"
-            clip-rule="evenodd"
-          />
+          {/if}
         </svg>
       </button>
-      <button type="button" on:click={clickLockScreen} class="lock-btn">
+    </div>
+    <div class="flex items-center justify-between">
+      <button
+        class="text-green-500 focus:outline-none"
+        on:click={clickLockScreen}
+        aria-label="Mute Button"
+      >
+        <!-- lock icon -->
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="2.2em"
-          height="2.2em"
-          viewBox="0 0 48 48"
-          {...$$props}
+          class="h-8 w-8"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
           <defs>
             <mask id="ipSUnlock0">
@@ -126,54 +114,36 @@
               </g>
             </mask>
           </defs>
-          <path fill="black" d="M0 0h48v48H0z" mask="url(#ipSUnlock0)" />
         </svg>
       </button>
-    {/if}
-  </div>
+    </div>
 
-  <div>
-    <!-- 이동 시간 -->
-    {formatSecToMMSS($elapsedTime)}
-    <div>
-      <!-- 움직인 거리 -->
-      이동거리:
-      {$totalDistance}
-      <!-- 평균 속도 -->
-      페이스:
-      {$currentPace}
-      <!-- 남은 거리 -->
-      {#if hasRoute}
-        남은 거리:
-      {/if}
+    <div class="text-center mt-12">
+      <div class="text-4xl font-bold text-gray-800">{formatSecToMMSS($elapsedTime)}</div>
+      <div class="mt-4 space-x-4 flex justify-center items-center">
+        <!-- Display dynamic data in an organized manner -->
+        <div class="text-sm text-gray-500">
+          이동거리: <span class="text-lg font-medium text-gray-700"
+            >{Math.round($totalDistance * 10) / 10} km</span
+          >
+        </div>
+        <div class="text-sm text-gray-500">
+          페이스: <span class="text-lg font-medium text-gray-700"
+            >{Math.round(3.4 * 10) / 10} km/h</span
+          >
+        </div>
+        {#if hasRoute}
+          <div class="text-sm text-gray-500">
+            남은 거리: <span class="text-lg font-medium text-gray-700"
+              >{Math.round(5.6 * 10) / 10} km</span
+            >
+          </div>
+        {/if}
+      </div>
     </div>
   </div>
 </div>
 
 <style>
-  /* .leaflet-bottom-center {
-    width: 100vw;
-  }
-  button {
-    left: 50%;
-    position: fixed;
-    transform: translate(-50%, 0%);
-  } */
-
-  /* 지도 안의 버튼 스타일 */
-  /* .lock-btn {
-    position: relative;
-    z-index: 100000; 
-    pointer-events: auto; 
-  } 
-  */
-  button {
-    width: 4rem;
-    height: 4rem;
-    border: 0;
-    background-color: transparent;
-    transition-property: background-color, background-opacity;
-    transition-duration: 250ms;
-    border-radius: 0.375rem;
-  }
+  /* Tailwind CSS already handles a lot of styling for you, but you can add custom styles here */
 </style>
--- FE/VincentRunGogh/src/pages/DrawingPage.svelte
+++ FE/VincentRunGogh/src/pages/DrawingPage.svelte
@@ -123,7 +123,35 @@
     }
   };
 
-  let gui = new L.Control({ position: 'bottomleft' });
+  L.Map.include({
+    _initControlPos: function () {
+      var corners = (this._controlCorners = {}),
+        l = 'leaflet-',
+        container = (this._controlContainer = L.DomUtil.create(
+          'div',
+          l + 'control-container',
+          this._container
+        ));
+
+      function createCorner(vSide, hSide) {
+        var className = l + vSide + ' ' + l + hSide;
+
+        corners[vSide + hSide] = L.DomUtil.create('div', className, container);
+      }
+
+      createCorner('top', 'left');
+      createCorner('top', 'right');
+      createCorner('bottom', 'left');
+      createCorner('bottom', 'right');
+
+      createCorner('top', 'center');
+      createCorner('middle', 'center');
+      createCorner('middle', 'left');
+      createCorner('middle', 'right');
+      createCorner('bottom', 'center');
+    },
+  });
+  let gui = new L.Control({ position: 'bottomcenter' });
   let guiComponent: MapGUI | null = null;
   gui.onAdd = (map: LeafletMap) => {
     const div = L.DomUtil.create('div');
@@ -201,7 +229,6 @@
 
     return div;
   };
-
   gui.onRemove = () => {
     if (guiComponent) {
       guiComponent.$destroy();
