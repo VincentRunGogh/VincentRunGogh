23f6b6e171a2aff0ca6060ff0a66a17fddf0d1d3
Jeon-YJ1004 <dudwn5374@naver.com>
2024-10-07 00:06:39 +0900

    [FEAT][FE][영주] bmi 뱃지

--- FE/VincentRunGogh/src/components/myhealth/BMIChart.svelte
+++ FE/VincentRunGogh/src/components/myhealth/BMIChart.svelte
@@ -1,36 +1,35 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { writable } from 'svelte/store';
+  import { Badge } from 'flowbite-svelte';
 
   const MIN_BMI = 15;
   const MAX_BMI = 40;
   export let bmi: number = 22.5; // 예제 BMI 값
 
   const bmiCategoryColors = {
-    Thinness: { bg: 'from-blue-200 to-blue-300', border: 'border-blue-300' },
-    Normal: { bg: 'from-green-200 to-green-300', border: 'border-green-300' },
-    Overweight: { bg: 'from-yellow-200 to-yellow-300', border: 'border-yellow-300' },
-    Obesity: { bg: 'from-red-200 to-red-300', border: 'border-red-300' },
+    Thinness: { bg: 'from-blue-200 to-blue-300', badge: 'indigo' },
+    Normal: { bg: 'from-green-200 to-green-300', badge: 'green' },
+    Overweight: { bg: 'from-yellow-200 to-yellow-300', badge: 'yellow' },
+    Obesity: { bg: 'from-red-200 to-red-300', badge: 'red' },
   };
 
   const groupList = [
     {
-      title: 'Thinness',
+      title: '저체중',
       range: [MIN_BMI, 18.5],
       color: bmiCategoryColors.Thinness,
     },
     {
-      title: 'Normal',
+      title: '정상',
       range: [18.5, 25],
       color: bmiCategoryColors.Normal,
     },
     {
-      title: 'Overweight',
+      title: '과체중',
       range: [25, 30],
       color: bmiCategoryColors.Overweight,
     },
     {
-      title: 'Obesity',
+      title: '비만',
       range: [30, MAX_BMI],
       color: bmiCategoryColors.Obesity,
     },
@@ -39,15 +38,33 @@
   const calculateWidth = (min: number, max: number): number => {
     return ((max - min) / (MAX_BMI - MIN_BMI)) * 100; // returns a percentage
   };
+  const markerPosition = (): string => {
+    if (bmi < MIN_BMI) return '0%'; // 최소 범위 이하일 때
+    if (bmi > MAX_BMI) return '100%'; // 최대 범위 이상일 때
 
-  onMount(() => {});
+    const position = ((bmi - MIN_BMI) / (MAX_BMI - MIN_BMI)) * 100;
+    return `${position}%`;
+  };
+  let currentCategory =
+    groupList.find((category) => bmi >= category.range[0] && bmi < category.range[1]) ??
+    groupList[0];
+  let markerStyle = `left: ${markerPosition()};`;
 </script>
 
-<div class="p-4 rounded-lg w-[90%]">
+<div class="p-4 rounded-lg w-[90%] h-[120px] flex flex-col items-center justify-between">
   <div class="text-3xl font-semibold text-center">{bmi}</div>
-  <div class="mt-4 flex w-full h-3 rounded overflow-hidden">
+  <div class="flex w-full h-3 rounded overflow-visible relative">
+    <div
+      class="absolute top-[-1.5rem] -translate-y-1/2 transform -translate-x-1/2"
+      style={markerStyle}
+    >
+      <Badge rounded large color={currentCategory.color.badge} class=" relative rounded-md  badge">
+        {currentCategory.title}
+      </Badge>
+    </div>
     {#each groupList as { range, title, color }}
       <div
+        id="hover"
         class={`flex-grow-0 h-full bg-gradient-to-r ${color.bg}`}
         style="width: {calculateWidth(range[0], range[1])}%"
       ></div>
@@ -56,5 +73,19 @@
 </div>
 
 <style>
-  /* Additional styles can be placed here */
+  .badge::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+    border-top-color: inherit;
+    border-bottom: 0;
+    margin-left: -10px;
+    margin-bottom: -10px;
+    z-index: 100; /* Z-index를 추가하여 보이도록 합니다 */
+    transform: translateY(100%); /* 꼬리가 보이도록 위치를 조정합니다 */
+  }
 </style>
