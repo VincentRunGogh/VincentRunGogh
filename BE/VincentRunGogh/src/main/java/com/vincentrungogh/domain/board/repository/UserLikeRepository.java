0ceb1775e4a076964e3607853fd45aca19a5d036
hyun-young <jhy7547@naver.com>
2024-10-06 18:59:12 +0900

    [FEAT][BE][현영] 사용자 좋아요 REPOSITORY

--- BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/board/repository/UserLikeRepository.java
+++ BE/VincentRunGogh/src/main/java/com/vincentrungogh/domain/board/repository/UserLikeRepository.java
@@ -1,13 +1,17 @@
 package com.vincentrungogh.domain.board.repository;
 
+import com.vincentrungogh.domain.board.entity.Board;
 import com.vincentrungogh.domain.board.entity.UserLike;
 import com.vincentrungogh.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserLikeRepository extends JpaRepository<UserLike, Integer> {
     
     List<UserLike> findAllByUser(User user);
 
+    // user-board like 관계 찾기
+    Optional<UserLike> findByUserAndBoard(User user, Board board);
 }
